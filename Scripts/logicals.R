983c118cff4518f0df1ca3aaae7ffa4c30770d8d
leeper <thosjleeper@gmail.com>
2013-09-19 22:45:41 +0200

    updated logicals.r

--- Scripts/logicals.R
+++ Scripts/logicals.R
@@ -1,1 +1,4 @@
 # Logicals
+
+# Logicals are a fundamental tool for using R in a sophisticated way.
+# Logicals allow us to precisely select elements of an R object (e.g., a vector or dataframe) based upon criteria and to selectively perform operations.
