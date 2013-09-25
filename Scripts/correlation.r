5ce8514482a7fb942dac7af0028f58b0437b2a7a
leeper <thosjleeper@gmail.com>
2013-09-25 21:21:52 +0200

    updated correlation plotting

--- Scripts/correlation.r
+++ Scripts/correlation.r
@@ -10,15 +10,39 @@ y <- 5*x1 + x2 + rnorm(n,1,2)
 
 # To obtain the correlation of two variables we simply list them consecutively:
 cor(x1,x2)
-# To obtian a correlation matrix, we have to supply an input matrix:
+# If we want to test the significance of the correlation, we need to use the `cor.test` function:
+cor.test(x1,x2)
+# To obtain a correlation matrix, we have to supply an input matrix:
 cor(cbind(x1,x2,y))
 
 
-# cor.test
 
-
-# TALK ABOUT CORRELATIONS OF NON-LINEAR RELATIONSHIPS.
-
+# Correlations of non-linear relationships
+a <- rnorm(n)
+b <- a^2 + rnorm(n)
+# If we plot the relationship of `b` on `a`, we see a strong (non-linear) relationship:
+plot(b~a)
+# Yet the correlation between the two variables is low:
+cor(a,b)
+cor.test(a,b)
+# If we can identify the functional form of the relationship, however, we can figure out what the relationship really is:
+plot(b~a, col='gray')
+# Clearly a linear relationship is inappropriate:
+curve((x), col='red', add=TRUE)
+# But what about `y ~ x^2` (of course, we know this is correct:
+curve((x^2), col='blue', add=TRUE)
+# The correlation between `b` and `a`^2 is thus much higher:
+cor(a^2,b)
+# We can see this visually by plotting `b` against the transformed `a` variable:
+plot(b ~ I(a^2), col='gray')
+# If we now overlay a linear relationship, we see how well the tranformed data are represented by a line:
+curve((x), col='blue', add=TRUE)
+# Now let's see this side-by-side to see how the transform works:
+layout(matrix(1:2,nrow=1))
+plot(b~a, col='gray')
+curve((x^2), col='blue', add=TRUE)
+plot(b ~ I(a^2), col='gray')
+curve((x), col='blue', add=TRUE)
 
 
 
