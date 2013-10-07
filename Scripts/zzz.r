a5ab099d7ff4334880289c6f681489173e185a92
Thomas J. Leeper <thosjleeper@gmail.com>
2013-10-07 14:54:21 +0200

    Create zzz.r

---
+++ Scripts/zzz.r
@@ -0,0 +1,19 @@
+#' # Build Index of Scripts #
+cat('# Index of R Course Scripts #\n\n', file='index.md')
+htmls <- dir(pattern='.html')
+htmls <- htmls[-grep('zzz.html',htmls)] # remove this script
+htmls <- htmls[-grep('index.html',htmls)] # remove the index
+mds <- gsub('.html','.md',htmls)
+titles <- sapply(mds, function(x) readLines(x,n=1))
+titles <- gsub('#','',titles)
+tmp <- data.frame(html=htmls,title=titles)
+tmp <- tmp[order(tmp$titles),]
+cat(paste('* [',tmp$title,'](',tmp$html,')',sep=''),sep='\n',file='index.md',append=TRUE)
+library(markdown)
+markdownToHTML('index.md',output='index.html')
+unlink('zzz.html') # delete this html
+unlink('zzz.md') # delete this md
+unlink('zzz.Rmd') # delete this Rmd
+unlink('*.md') # delete-all md
+unlink('*.Rmd') # delete-all Rmd
+browseURL('index.html')
