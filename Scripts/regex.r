610f745953ade079b2b667c31a0b11912ecf190b
Thomas J. Leeper <thosjleeper@gmail.com>
2013-11-06 14:07:15 +0100

    Create regex.r

---
+++ Scripts/regex.r
@@ -0,0 +1,30 @@
+#' # Regular expressions #
+
+as.character()
+toupper()
+tolower()
+
+
+match()
+pmatch()
+
+
+? regex
+[Regular Expressions (Wikipedia)](http://en.wikipedia.org/wiki/Regular_expression)
+
+grep()
+grepl()
+
+regexpr()
+gregexpr()
+regexec()
+
+agrep()
+
+
+regmatches()
+
+
+sub()
+gsub()
+
