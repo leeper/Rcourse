b69aa88eed947224b845b31ec69620d97c6b2978
Thomas J. Leeper <thosjleeper@gmail.com>
2013-11-07 17:30:08 +0100

    Create nominalglm.r

---
+++ Scripts/nominalglm.r
@@ -0,0 +1,6 @@
+#' # Multinomial Outcome Models #
+#' 
+#' One important, but sometimes problematic, class of regression models deals with nominal or multinomial outcomes (i.e., outcomes that are not continuous or even ordered). Estimating these models is not possible with `glm`, but can be estimated using the *nnet* add-on package, which is recommended and therefore simply needs to be loaded.
+
+library(nnet)
+# multinom()
