befa02a23d6d98150928efda602d233fa1e8e778
Thomas J. Leeper <thosjleeper@gmail.com>
2013-11-07 17:24:01 +0100

    Create countglm.r

---
+++ Scripts/countglm.r
@@ -0,0 +1,10 @@
+#' # Count Regression Models #
+#'
+#' We sometimes want to estimate models of count outcomes. Depending on substantive assumptions, we can model these using a linear model, an ordered outcome model, or a count-specific model. This tutorial talks about count models, specifically poisson models and negative beta binomial models.
+#' Poisson models can be estimated using R's base `glm` function, but negative beta binomial regression requires teh MASS add-on package, which is a recommended and therefore is pre-installed and you simply need to load it.
+
+#poisson(link = "log")
+
+library(MASS)
+#glm.nb()
+
