4d71cddda40635487b0a7336258616570d82740b
Thomas J. Leeper <thosjleeper@gmail.com>
2013-11-07 17:27:16 +0100

    Create orderedglm.r

---
+++ Scripts/orderedglm.r
@@ -0,0 +1,5 @@
+#' # Ordered Outcome Models #
+#'
+#' This tutorial focues on ordered outcome regression models. R's base `glm` function does not support these, but they're very easy to execute using the MASS package, which is a recommended package.
+
+#polr()
