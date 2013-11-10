8089cfbec5c6fc889b98c16b6b52e5c5c058b66a
leeper <thosjleeper@gmail.com>
2013-11-10 22:15:13 +0100

    update spin cleanup

--- Scripts/zzz.r
+++ Scripts/zzz.r
@@ -1,18 +1,15 @@
 #' # Build Index of Scripts #
-#getwd()
-cat('# Index of R Course Scripts #\n\n', file='index.md')
+cat('# Index of R Tutorials #\n\n', file='index.md')
 htmls <- dir(pattern='.r$')
 htmls <- gsub('.r','.html',htmls, fixed=TRUE)
 z <- grep('zzz.html',htmls)
 if(length(z))
     htmls <- htmls[-z] # remove this script
-#htmls
 mds <- gsub('.html','.md',htmls)
 titles <- sapply(mds, function(x) readLines(x,n=1))
 titles <- gsub("[#']",'',titles)
 tmp <- data.frame(html=htmls,title=titles)
 tmp <- tmp[order(tmp$title),]
-#tmp
 cat(paste('* [',tmp$title,'](',tmp$html,')',sep=''),sep='\n',file='index.md',append=TRUE)
 library(markdown)
 markdownToHTML('index.md',output='index.html')
@@ -20,12 +17,12 @@ markdownToHTML('index.md',output='index.html')
 unlink('*.md') # delete-all md
 unlink('*.Rmd') # delete-all Rmd
 
-if('HTML' %in% dir())
-    unlink('./HTML',recursive=TRUE)
-dir.create('./HTML')
+if('Tutorials' %in% dir('../'))
+    unlink('../Tutorials',recursive=TRUE)
+dir.create('../Tutorials')
 infiles <- dir(pattern='.html')
-outfiles <- file.path(file.path(getwd(),'HTML'),infiles)
+outfiles <- file.path('../Tutorials',infiles)
 file.rename(infiles,outfiles)
-file.rename('figure','./HTML/figure')
+file.rename('figure','../Tutorials/figure')
 
-browseURL(file.path(getwd(),'HTML/index.html'))
+browseURL(file.path(getwd(),'../Tutorials/index.html'))
