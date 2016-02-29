2b64cf0d2bd10dc3e7f711d0f1d5188854cbe270
Thomas J. Leeper <thosjleeper@gmail.com>
2016-02-29 16:15:24 +0000

    Update CusackIversenSoskiceAPSR2007.R

--- Data/CusackIversenSoskiceAPSR2007.R
+++ Data/CusackIversenSoskiceAPSR2007.R
@@ -1,8 +1,7 @@
-# Replication of Cusack, Iversen, and Soskice (2007)
+# Reproduction and Extension of Cusack, Iversen, and Soskice (2007)
 
-
-library(foreign)
-cis <- read.dta('CusackIversenSoskiceAPSR2007.dta')
+library("rio")
+cis <- import("CusackIversenSoskiceAPSR2007.dta")
 
 # Table 3
 # Model 1
@@ -35,4 +34,46 @@ lm5_6 <- lm(thresh ~ dispro2,
 # Get all regression results for all models
 sapply(ls(pattern='lm'), function(obj) round(summary(get(obj))$coef[,1:2],2))
 
+
+# models combining all right-hand side variables
+lm_extra1 <- lm(thresh ~ stthroct2 + coordds + dispro2 + fragdum,
+    data=cis[cis$smpl==1 & !is.na(cis$coordds),])
+lm_extra2 <- lm(thresh ~ stthroct2 + coordds + dispro2 + fragdum,
+    data=cis[cis$smpl==1 & cis$oursmpl==1 & !is.na(cis$dispro2) & !cis$cow==220,])
+
+# models also accounting for British colonial history
+cis$brit <- 
+c(1, # USA
+1, # Canada
+1, # UK
+1, # Ireland
+0, # Netherlands
+0, # Belgium
+0, # Luxembourg
+0, # France
+0, # Switzerland
+0, # Spain
+0, # Germany
+0, # Austria
+0, # Italy
+0, # Greece
+0, # Finland
+0, # Sweden
+0, # Norway
+0, # Denmark
+0, # Iceland
+0, # Japan
+1, # Australia
+1, # New Zealand
+rep(0, 9)) # duplicated countries
+
+lm_extra3 <- lm(thresh ~ stthroct2 + coordds + dispro2 + fragdum + brit,
+    data=cis[cis$smpl==1 & !is.na(cis$coordds),])
+lm_extra4 <- lm(thresh ~ stthroct2 + coordds + dispro2 + fragdum + brit,
+    data=cis[cis$smpl==1 & cis$oursmpl==1 & !is.na(cis$dispro2) & !cis$cow==220,])
+
+summary(lm_extra1)
+summary(lm_extra2)
+summary(lm_extra3)
+summary(lm_extra4)
 
